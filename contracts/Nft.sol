7a48dc7696afdcfa63a768f32cda5609ee3e66a9
Gabriel Ayala <gabeayala100@gmail.com>
2022-05-07 13:22:40 -0700

    Change the contract name and import openzeppelins dependences.

---
+++ contracts/Nft.sol
@@ -0,0 +1,10 @@
+// SPDx-License-Identifyer: MIT
+pragma solidity ^0.8.4;
+
+// this allows us to use contracts from openzeppelin which give us usable code
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT {
+    //Create an ERC721 contract
+    //Mint some NFTS
+}
