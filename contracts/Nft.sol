06d96fdd5050d75adbff9c8597e4b92098eb8181
Gabriel Ayala <gabeayala100@gmail.com>
2022-05-07 13:32:01 -0700

    create a constructor for minting the nft and compile the contract.

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -4,7 +4,12 @@ pragma solidity ^0.8.4;
 // this allows us to use contracts from openzeppelin which give us usable code
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract NFT {
+//this denotes that what type of contract framework
+contract NFT is ERC721{
     //Create an ERC721 contract
     //Mint some NFTS
+
+    constructor () ERC721("Learn NFT", "LNFT"){
+        _mint (msg.sender, 1);
+    }
 }
